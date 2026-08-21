8cf78b112d359c1a28f8be31f63a9fee935b2148
YiHx <yhx2582413@gmail.com>
2026-08-21 22:37:00 +0800

    Complete testThreeAddThreeRemove method

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,6 +1,7 @@
 package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -8,5 +9,22 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+  @Test
+    public void testThreeAddThreeRemove(){
+      AListNoResizing<Integer> test1 =new AListNoResizing<>();
+      BuggyAList<Integer> test2 = new BuggyAList<>();
+      test1.addLast(1);
+      test1.addLast(2);
+      test1.addLast(3);
+
+      test2.addLast(1);
+      test2.addLast(2);
+      test2.addLast(3);
+
+      assertEquals(test1.removeLast(),test2.removeLast());
+      assertEquals(test1.removeLast(),test2.removeLast());
+      assertEquals(test1.removeLast(),test2.removeLast());
+
+
+  }
 }
