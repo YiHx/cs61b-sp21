64a252505af56f1038ae87ba893a6c0e0a127b96
YiHx <yhx2582413@gmail.com>
2026-08-23 22:22:16 +0800

    update

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,4 +1,11 @@
 package deque;
 
-public class LinkedListDeque {
+public class LinkedListDeque<Item> {
+    public int size;
+
+    public LinkedListDeque(){
+        size=0;
+        LinkedListDeque<Item> = new
+    }
+
 }
