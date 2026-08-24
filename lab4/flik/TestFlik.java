453eed1667c511c28a4959fc2ebd5fca487fc424
YiHx <yhx2582413@gmail.com>
2026-08-24 20:59:59 +0800

    Using good style

--- lab4/flik/TestFlik.java
+++ lab4/flik/TestFlik.java
@@ -1,24 +1,25 @@
 package flik;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
 public class TestFlik {
 
     @Test
-    public void test1(){
-        assertTrue(Flik.isSameNumber(1,1));
+    public void test1() {
+        assertTrue(Flik.isSameNumber(1, 1));
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         assertFalse(Flik.isSameNumber(1, 2));
     }
 
     @Test
-    public void test3(){
-        assertTrue(Flik.isSameNumber(128,128));
+    public void test3() {
+        assertTrue(Flik.isSameNumber(128, 128));
     }
 
 }
