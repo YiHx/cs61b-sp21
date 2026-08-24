ee79e9264c3ed924901364eb9e5088d90b20f4ec
YiHx <yhx2582413@gmail.com>
2026-08-24 20:53:45 +0800

    Fix Bug

---
+++ lab4/flik/TestFlik.java
@@ -0,0 +1,24 @@
+package flik;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class TestFlik {
+
+    @Test
+    public void test1(){
+        assertTrue(Flik.isSameNumber(1,1));
+    }
+
+    @Test
+    public void test2(){
+        assertFalse(Flik.isSameNumber(1, 2));
+    }
+
+    @Test
+    public void test3(){
+        assertTrue(Flik.isSameNumber(128,128));
+    }
+
+}
