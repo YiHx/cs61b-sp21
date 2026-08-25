24cda3e7f84d36a905df80246799fbcb733931e1
YiHx <yhx2582413@gmail.com>
2026-08-25 23:43:38 +0800

    Complete ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,86 +1,133 @@
 package deque;
 
-public class ArrayDeque<T> {
+import afu.org.checkerframework.checker.oigj.qual.O;
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements Iterable<T>{
     private T[] array;
     private int size;
-    private int End;
-    private int First;
+    private int nextEnd;
+    private int nextFirst;
 
     public ArrayDeque() {
         array = (T[]) new Object[8];
         size = 0;
-        End = 0;
-        First = 0;
+        nextEnd = 0;
+        nextFirst = 1;
     }
 
-    public void resize(){
-        if(size/array.length>1/4){
-            T[] curr = (T[])new Object[array.length*2];
-            int begin =First-1;
-            int i=0;
-            for(;begin<array.length;i++,begin++){
-                curr[i]=array[begin];
-            }
-            for(int j=i+1,k=0;k<End;j++,k++){
-                curr[j]=array[k];
-            }
-            array=curr;
+    private class ArrayDequeIterator implements Iterator<T>{
+        int curr;
+        public ArrayDequeIterator(){
+            curr=0;
+        }
+        public boolean hasNext(){
+            return curr < size;
+        }
+
+        public T next(){
+            T returnValue = get(curr);
+            curr++;
+            return returnValue;
         }
     }
 
-    public void addFirst(T item){
-        resize();
-        if(array[0]==null){
-            array[First]=item;
-            First=array.length;
-            End++;
-            size++;
-            return;
+    private void resize(int capacity) {
+        T[] newArray = (T[]) new Object[capacity];
+        int curr = (nextFirst + 1) % array.length;
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[curr];
+            curr = (curr + 1) % array.length;
         }
-        array[First]=item;
+        array = newArray;
+        nextFirst = capacity - 1;
+        nextLast = size;
+    }
+
+
+    private void checkCapacity() {
+        if (size == array.length) {
+            resize(array.length * 2);
+        } else if (array.length >= 16 && (double) size / array.length < 0.25) {
+            resize(array.length / 2);
+        }
+    }
+
+
+    public void addFirst(T item) {
+        checkCapacity();
+        array[nextFirst] = item;
+        nextFirst = (nextFirst - 1 + array.length) % array.length;
         size++;
-        First--;
-
     }
-    public void addLast(T item){
-        resize();
-        if(array[0]==null){
-            array[End]=item;
-            End++;
-            size++;
-            First=array.length;
-            return;
-        }
-        array[End] = item;
+
+    public void addLast(T item) {
+        checkCapacity();
+        array[nextEnd] = item;
+        nextEnd = (nextEnd + 1) % array.length;
         size++;
-        End++;
     }
 
-    public boolean isEmpty(){
-        return size==0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
-        if(First+1!=array.length){
-            for(int i=First+1;i<array.length;i++) {
-                System.out.print(array[i] + " ");
-            }
+    public void printDeque() {
+        int curr = (nextFirst + 1) % array.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[curr]);
+            curr = (curr + 1) % array.length;
         }
-        if(End-1>=0){
-            for(int i=End-1;array[i]!=null;i++){
-                System.out.print(array[i]+" ");
-            }
-        }
-        System.out.print("\n");
+        System.out.println();
     }
 
-    public T removeFirst(){
-
+    public T removeFirst() {
+        if (isEmpty()) return null;
+        T item = array[(nextFirst + 1) % array.length];
+        nextFirst = (nextFirst + 1) % array.length;
+        size--;
+        return item;
     }
+
+    public T removeLast() {
+        if (isEmpty()) return null;
+        T item = arry(nextEnd - 1 + array.length) % array.length;
+        nextEnd = (nextEnd - 1 + array.length) % array.length;
+        return item;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return array[(nextFirst + 1 + index) % array.length];
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayDequeIterator();
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof ArrayDeque){
+            ArrayDeque<T> curr =(ArrayDeque<T>) o;
+            if(curr.size!=this.size)return false;
+            for(int i=0;i<size;i++){
+                if(curr.get(i)!=this.get(i))return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+
 
 
 }
