00a13a7a0b4b4f618baa1c9e04b5b2b456f27c74
YiHx <yhx2582413@gmail.com>
2026-09-03 15:12:20 +0800

    fix bug

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,22 +1,22 @@
 package hashmap;
 
-import org.junit.Test;
 
 import java.util.*;
 
 /**
- *  A hash table-backed Map implementation. Provides amortized constant time
- *  access to elements via get(), remove(), and put() in the best case.
+ * A hash table-backed Map implementation. Provides amortized constant time
+ * access to elements via get(), remove(), and put() in the best case.
+ * <p>
+ * Assumes null keys will never be inserted, and does not resize down upon remove().
  *
- *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author xuanh08
+ * @author xuanh08
  */
-    public class    MyHashMap<K, V> implements Map61B<K, V> {
+public class MyHashMap<K, V> implements Map61B<K, V> {
 
-        private int initialSize ;
-        private double  loadFactor;
-        private Collection<Node>[] table;
-        private  int size =0;
+    private int initialSize;
+    private double loadFactor;
+    private Collection<Node>[] table;
+    private int size = 0;
 
 
 //    @Override
@@ -42,16 +42,18 @@ import java.util.*;
     private Collection<Node>[] buckets;
     // You should probably define some more!
 
-    /** Constructors */
+    /**
+     * Constructors
+     */
     public MyHashMap() {
-        initialSize =16;
-        loadFactor =0.75;
+        initialSize = 16;
+        loadFactor = 0.75;
         table = createTable(initialSize);
     }
 
     public MyHashMap(int initialSize) {
-        this.initialSize =initialSize;
-        loadFactor =0.75;
+        this.initialSize = initialSize;
+        loadFactor = 0.75;
         table = createTable(initialSize);
     }
 
@@ -60,10 +62,10 @@ import java.util.*;
      * The load factor (# items / # buckets) should always be <= loadFactor
      *
      * @param initialSize initial size of backing array
-     * @param maxLoad maximum load factor
+     * @param maxLoad     maximum load factor
      */
     public MyHashMap(int initialSize, double maxLoad) {
-        this.initialSize =initialSize;
+        this.initialSize = initialSize;
         this.loadFactor = maxLoad;
         table = createTable(initialSize);
     }
@@ -77,19 +79,19 @@ import java.util.*;
 
     /**
      * Returns a data structure to be a hash table bucket
-     *
+     * <p>
      * The only requirements of a hash table bucket are that we can:
-     *  1. Insert items (`add` method)
-     *  2. Remove items (`remove` method)
-     *  3. Iterate through items (`iterator` method)
-     *
+     * 1. Insert items (`add` method)
+     * 2. Remove items (`remove` method)
+     * 3. Iterate through items (`iterator` method)
+     * <p>
      * Each of these methods is supported by java.util.Collection,
      * Most data structures in Java inherit from Collection, so we
      * can use almost any data structure as our buckets.
-     *
+     * <p>
      * Override this method to use different data structures as
      * the underlying bucket type
-     *
+     * <p>
      * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
@@ -100,24 +102,24 @@ import java.util.*;
     /**
      * Returns a table to back our hash table. As per the comment
      * above, this table can be an array of Collection objects
-     *
+     * <p>
      * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
      * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
      *
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
-        Collection<Node>[] table = (Collection<Node>[]) new Collection[tableSize];
+        Collection<Node>[] newTable = (Collection<Node>[]) new Collection[tableSize];
         for (int i = 0; i < tableSize; i++) {
-            table[i] = createBucket();
+            newTable[i] = createBucket();
         }
-        return table;
+        return newTable;
     }
 
 
-    public void clear(){
+    public void clear() {
         table = createTable(this.initialSize);
-        size=0;
+        size = 0;
     }
 
     public boolean containsKey(K key) {
@@ -132,36 +134,36 @@ import java.util.*;
     }
 
 
-    public V get(K key){
-        if(!containsKey(key)){
+    public V get(K key) {
+        if (!containsKey(key)) {
             return null;
         }
 
-        int number = Math.floorMod(key.hashCode(),initialSize);
-        for(Node it : table[number]){
-            if(it.key.equals(key)){
+        int number = Math.floorMod(key.hashCode(), initialSize);
+        for (Node it : table[number]) {
+            if (it.key.equals(key)) {
                 return it.value;
             }
         }
         return null;
     }
 
-    public int size(){
+    public int size() {
         return this.size;
     }
 
-    public void put(K key,V value){
-        int number = Math.floorMod(key.hashCode(),initialSize);
-        Node curr = createNode(key,value);
-        if(table[number].isEmpty()){
+    public void put(K key, V value) {
+        int number = Math.floorMod(key.hashCode(), initialSize);
+        Node curr = createNode(key, value);
+        if (table[number].isEmpty()) {
             table[number].add(curr);
             size++;
             resize();
-        }else{
-            for(Node it:table[number]){
-                if(it.key.equals(key)){
-                    it.value=value;
-                    return ;
+        } else {
+            for (Node it : table[number]) {
+                if (it.key.equals(key)) {
+                    it.value = value;
+                    return;
                 }
 
             }
@@ -172,8 +174,8 @@ import java.util.*;
         }
     }
 
-    public Set<K> keySet(){
-        Set<K> now =  new HashSet<>();
+    public Set<K> keySet() {
+        Set<K> now = new HashSet<>();
         for (Collection<Node> nodes : table) {
             if (nodes.isEmpty()) {
                 continue;
@@ -187,20 +189,18 @@ import java.util.*;
     }
 
 
-
-
-    public  V remove(K key, V value){
-        if(key==null){
+    public V remove(K key, V value) {
+        if (key == null) {
             return null;
         }
 
-        int number = Math.floorMod(key.hashCode(),initialSize);
-        Node now = createNode(key,value);
-        if(table[number].isEmpty()){
+        int number = Math.floorMod(key.hashCode(), initialSize);
+        Node now = createNode(key, value);
+        if (table[number].isEmpty()) {
             return null;
         }
-        for(Node it :table[number]){
-            if(it.equals(now)){
+        for (Node it : table[number]) {
+            if (it.equals(now)) {
                 V val = it.value;
                 size--;
                 table[number].remove(now);
@@ -209,40 +209,41 @@ import java.util.*;
         }
         return null;
     }
-    public V remove (K key){
+
+    public V remove(K key) {
         if (key == null) {
             return null;
         }
-        int number = Math.floorMod(key.hashCode(),initialSize);
+        int number = Math.floorMod(key.hashCode(), initialSize);
         Iterator<Node> it = table[number].iterator();
-            while(it.hasNext()){
-                Node node =it.next();
-                if(node.key.equals(key)){
-                    V val = node.value;
-                    it.remove();
-                    size--;
-                    return val;
-                }
+        while (it.hasNext()) {
+            Node node = it.next();
+            if (node.key.equals(key)) {
+                V val = node.value;
+                it.remove();
+                size--;
+                return val;
             }
-            return null;
         }
+        return null;
+    }
 
 //    public V remove(K key, V value){
 //        throw new UnsupportedOperationException();
 //
 //    }
 
-    public void resize(){
-        if((double)size/(double)initialSize>loadFactor){
+    private void resize() {
+        if ((double) size / (double) initialSize > loadFactor) {
             Collection<Node>[] curr = table;
-            table = createTable(initialSize*2);
-            initialSize =initialSize*2;
-            for(Collection<Node> nodes : curr){
-                if(nodes.isEmpty()){
+            table = createTable(initialSize * 2);
+            initialSize = initialSize * 2;
+            for (Collection<Node> nodes : curr) {
+                if (nodes.isEmpty()) {
                     continue;
                 }
-                for(Node now : nodes){
-                    int number = Math.floorMod(now.key.hashCode(),initialSize);
+                for (Node now : nodes) {
+                    int number = Math.floorMod(now.key.hashCode(), initialSize);
                     table[number].add(now);
                 }
             }
@@ -250,7 +251,7 @@ import java.util.*;
     }
 
 
-    public Iterator<K> iterator(){
+    public Iterator<K> iterator() {
         return keySet().iterator();
     }
 
